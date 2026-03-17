3c3e3a413b1cab27e34912179a721199acaa70d5
Sofya <157707898+SofyaSelivon@users.noreply.github.com>
2026-03-17 16:28:19 +0300

    Update main.py

--- seller_main/app/main.py
+++ seller_main/app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from app.routes.market_routes import router as market_router
 from app.routes.product_routes import router as product_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(market_router)
 app.include_router(product_router)
